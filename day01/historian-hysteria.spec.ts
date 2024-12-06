c31ef513d9342f5b746ee45172234d629d31bc08
Florian Hamel <fhamel.freelance@gmail.com>
2024-12-06 21:23:52 +0100

    day01: ⭐️⭐️

--- day01/historian-hysteria.spec.ts
+++ day01/historian-hysteria.spec.ts
@@ -1,7 +1,7 @@
-import { solve } from './historian-hysteria';
+import { solve1, solve2 } from './historian-hysteria';
 
 describe('historian hysteria', () => {
-  it('should find sum of diffs', async () => {
+  it('should find sum of diffs', () => {
     // Given
     const input =
       '3   4\n' +
@@ -12,9 +12,26 @@ describe('historian hysteria', () => {
       '3   3';
 
     // When
-    const result = await solve(input);
+    const result = solve1(input);
 
     // Then
-    expect(result).toEqual(11)
+    expect(result).toEqual(11);
   });
-}); 
+
+  it('should calculate similarity score', () => {
+    // Given
+    const input =
+      '3   4\n' +
+      '4   3\n' +
+      '2   5\n' +
+      '1   3\n' +
+      '3   9\n' +
+      '3   3';
+
+    // When
+    const result = solve2(input);
+
+    // Then
+    expect(result).toEqual(31);
+  });
+});
--- day01/historian-hysteria.ts
+++ day01/historian-hysteria.ts
@@ -1,11 +1,11 @@
 import { readPuzzleInput } from '../utils/file-input-reader';
 
-export function solve(input: string): number {
+export function solve1(input: string): number {
   const rows = input.trim().split('\n');
   const l1: number[] = [];
   const l2: number[] = [];
   const diffs: number[] = [];
-  
+
   rows.forEach((row) => {
     const splitRow = row.split('   ');
     l1.push(+splitRow[0]);
@@ -13,7 +13,7 @@ export function solve(input: string): number {
   });
   l1.sort();
   l2.sort();
-  
+
   for (let i = 0; i < l1.length && i < l2.length; ++i) {
     diffs.push(Math.abs(l1[i] - l2[i]));
   }
@@ -21,7 +21,27 @@ export function solve(input: string): number {
   return diffs.reduce((acc, curr) => acc + curr, 0);
 }
 
+export function solve2(input: string): number {
+  const rows = input.trim().split('\n');
+  const l1: number[] = [];
+  const l2: number[] = [];
+  let score: number = 0;
+
+  rows.forEach((row) => {
+    const splitRow = row.split('   ');
+    l1.push(+splitRow[0]);
+    l2.push(+splitRow[1]);
+  });
+
+  l1.forEach(val => {
+    score += val * l2.filter(nb => nb === val).length;
+  });
+
+  return score;
+}
+
 (async () => {
   const input = await readPuzzleInput('./puzzle-input.txt');
-  console.log((input));
-})()
+  console.log((solve1(input)));
+  console.log((solve2(input)));
+})();
