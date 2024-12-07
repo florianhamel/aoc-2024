5a3dc9d3c9c3852f63a009783acc5dcd43b1f211
Florian Hamel <fhamel.freelance@gmail.com>
2024-12-07 20:47:12 +0100

    day02: ⭐️⭐️

--- day02/red-nosed-reports.spec.ts
+++ day02/red-nosed-reports.spec.ts
@@ -1,5 +1,4 @@
 import { redNosedReports } from './red-nosed-reports';
-import solve1 = redNosedReports.solve1;
 
 describe('red-nosed reports', () => {
 
@@ -14,10 +13,40 @@ describe('red-nosed reports', () => {
       '1 3 6 7 9';
 
     // when
-    const result = solve1(input);
+    const result = redNosedReports.solve1(input);
 
     // then
     expect(result).toEqual(2);
   });
 
+  it('should count safe reports modulo one bad level', () => {
+    // given
+    const input =
+      '7 6 4 2 1\n' +
+      '1 2 7 8 9\n' +
+      '9 7 6 2 1\n' +
+      '1 3 2 4 5\n' +
+      '8 6 4 4 1\n' +
+      '1 3 6 7 9\n' +
+      '40 38 42 45 48\n' +
+      '10 7 24 5 3 1\n' +
+      '35 37 33 30 27\n' +
+      '30 30 31 34\n' +
+      '48 46 47 49 51 54 56\n' +
+      '1 1 2 3 4 5\n' +
+      '1 2 3 4 5 5\n' +
+      '5 1 2 3 4 5\n' +
+      '1 4 3 2 1\n' +
+      '1 6 7 8 9\n' +
+      '1 2 3 4 3\n' +
+      '9 8 7 6 7\n' +
+      '7 10 8 10 11\n' +
+      '29 28 27 25 26 25 22 20';
+
+    // when
+    const result = redNosedReports.solve2(input);
+
+    // then
+    expect(result).toEqual(18);
+  });
 });
--- day02/red-nosed-reports.ts
+++ day02/red-nosed-reports.ts
@@ -6,7 +6,6 @@ export namespace redNosedReports {
     const lines = input.trim().split('\n');
     lines.forEach(l => {
       const report = l.split(' ').map(n => +n);
-      console.log(report);
       if (isSafeReport(report)) {
         ++safeReports;
       }
@@ -22,17 +21,88 @@ export namespace redNosedReports {
     const isAsc = report[1] - report[0] > 0;
     for (let i = 1; i < report.length; ++i) {
       const diff = report[i] - report[i - 1];
-      if (isAsc && (diff > 3 || 1 > diff)) {
+      if (isBadData(isAsc, diff)) {
         return false;
-      } else if (!isAsc && diff > -1 || -3 > diff) {
-        return false
       }
     }
+
     return true;
+  }
+
+  function isBadData(isAsc: boolean, diff: number): boolean {
+    if (isAsc && (diff > 3 || 1 > diff)) {
+      return true;
+    }
+
+    return !isAsc && (diff > -1 || -3 > diff);
+  }
+
+  export function solve2(input: string): number {
+    let safeReports = 0;
+    const lines = input.trim().split('\n');
+    lines.forEach(l => {
+      const report = l.split(' ').map(n => +n);
+      if (isReportAsc(report) || isReportDesc(report)) {
+        ++safeReports;
+      }
+    });
+
+    return safeReports;
+  }
+
+  function isReportAsc(report: number[]): boolean {
+    let isJokerUsed = false;
+    for (let i = 1; i < report.length; ++i) {
+      if (!isAsc(report[i] - report[i - 1])) {
+        if (isJokerUsed) {
+          return false;
+        }
+        if (isAsc(report[i + 1] - report [i - 1])) {
+          report = report.toSpliced(i, 1);
+        } else {
+          if (i !== 1 && i !== report.length - 1 && !isAsc(report[i] - report[i - 2])) {
+            return false;
+          }
+        }
+        isJokerUsed = true;
+      }
+    }
+
+    return true;
+  }
+
+  function isReportDesc(report: number[]): boolean {
+    let isJokerUsed = false;
+    for (let i = 1; i < report.length; ++i) {
+      if (!isDesc(report[i] - report[i - 1])) {
+        if (isJokerUsed) {
+          return false;
+        }
+        if (isDesc(report[i + 1] - report [i - 1])) {
+          report = report.toSpliced(i, 1);
+        } else {
+          if (i !== 1 && i !== report.length - 1 && !isDesc(report[i] - report[i - 2])) {
+            return false;
+          }
+        }
+        isJokerUsed = true;
+      }
+    }
+
+    return true;
+  }
+
+  function isAsc(diff: number): boolean {
+    return 4 > diff && diff > 0;
+  }
+
+  function isDesc(diff: number): boolean {
+    return 0 > diff && diff > -4;
   }
 }
 
 (async () => {
   const input = await readPuzzleInput('./puzzle-input.txt');
   console.log(redNosedReports.solve1(input));
-})()
+  console.log(redNosedReports.solve2(input));
+})();
