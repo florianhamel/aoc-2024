75d4a9c2f8ef216035894d8770224c11c490fad6
Florian Hamel <fhamel.freelance@gmail.com>
2024-12-08 10:05:14 +0100

    day03: ⭐️⭐️

--- day03/mull-it-over.spec.ts
+++ day03/mull-it-over.spec.ts
@@ -11,4 +11,15 @@ describe('mull it over', () => {
     // then
     expect(result).toEqual(161);
   });
+
+  it('should add up multiplication results with do and don\'t', () => {
+    // given
+    const input = 'xmul(2,4)&mul[3,7]!^don\'t()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))';
+
+    // when
+    const result = MullItOver.solve2(input);
+
+    // then
+    expect(result).toEqual(48);
+  });
 });
--- day03/mull-it-over.ts
+++ day03/mull-it-over.ts
@@ -2,9 +2,8 @@ import { readPuzzleInput } from '../utils/file-input-reader';
 
 export namespace MullItOver {
   export function solve1(input: string): number {
-    const multiplications = input.match(/mul\([0-9]{0,3},[0-9]{1,3}\)/gm);
+    const multiplications = input.match(/mul\([0-9]{1,3},[0-9]{1,3}\)/g);
     let sum = 0;
-    console.log(multiplications);
     multiplications?.forEach(m => {
       const [a, b] = m.match(/[0-9]{1,3}/g)!.map(n => +n);
       sum += a * b;
@@ -12,9 +11,32 @@ export namespace MullItOver {
 
     return sum;
   }
+
+  export function solve2(input: string): number {
+    const matches = input.match(/mul\([0-9]{1,3},[0-9]{1,3}\)|don't\(\)|do\(\)/g);
+    let sum = 0;
+    let enabled = true;
+    matches?.forEach(m => {
+      const reDoNot = /don't\(\)/;
+      const reDo = /do\(\)/;
+
+      if (reDoNot.test(m)) {
+        enabled = false;
+      } else {
+        if (reDo.test(m)) {
+          enabled = true;
+        } else {
+          const [a, b] = m.match(/[0-9]{1,3}/g)!.map(n => +n);
+          sum += enabled ? a * b : 0;
+        }
+      }
+    });
+    return sum;
+  }
 }
 
 (async () => {
   const input = await readPuzzleInput('./puzzle-input.txt');
   console.log(MullItOver.solve1(input));
+  console.log(MullItOver.solve2(input));
 })()
